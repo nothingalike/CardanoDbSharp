f22e57ee16d4ce25cac32b95689e325e7d50d019
Kyle Johns <kjohns1337@gmail.com>
2020-12-24 23:15:51 -0500

    four new repositories

---
+++ Repositories/TxMetadatum.cs
@@ -0,0 +1,20 @@
+﻿using CardanoDbSharp.Common;
+using CardanoDbSharp.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace CardanoDbSharp.Repositories
+{
+    public interface ITxMetadatumRepository: IRepository<TxMetadatum>
+    {
+    }
+    public class TxMetadatumRepository: Repository<TxMetadatum>, ITxMetadatumRepository
+    {
+        public TxMetadatumRepository(IConfiguration configuration) 
+            : base(configuration, TableNames.TxMetadatum) { }
+    }
+}
---
+++ Repositories/UtxoViewRepository.cs
@@ -0,0 +1,20 @@
+﻿using CardanoDbSharp.Common;
+using CardanoDbSharp.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace CardanoDbSharp.Repositories
+{   
+    public interface IUtxoViewRepository: IRepository<UtxoView>
+    {
+    }
+    public class UtxoViewRepository: Repository<UtxoView>, IUtxoViewRepository
+    {
+        public UtxoViewRepository(IConfiguration configuration) 
+            : base(configuration, TableNames.UtxoView) { }
+    }
+}
